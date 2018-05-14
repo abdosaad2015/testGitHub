dec8442a7d0865f7fdd5ddae27b1e5e9de43abcc
abdosaad2015 <abdosaad2015@gmail.com>
2018-05-14 17:58:47 +0200

    Hello Fouad

--- app/src/main/java/com/abdo/saad/testgithub/MainActivity.java
+++ app/src/main/java/com/abdo/saad/testgithub/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //HELLO FOUAD
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
